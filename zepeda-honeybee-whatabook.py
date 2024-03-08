0dfbbcb57ba8ca4c475d95c52f3f5ee67fefe321
Evelyn Zepeda <evaynnn@gmail.com>
2024-03-07 18:24:13 -0800

    Python Update

--- zepeda-honeybee-whatabook.py
+++ zepeda-honeybee-whatabook.py
@@ -7,6 +7,7 @@
 #To import MongoClient from pymongo
 from pymongo import MongoClient
 
+
 #A string to connect to the database
 conn = "mongodb+srv://whatabook_admin:s3cret@bellevueuniversity.8vzftv7.mongodb.net/whatabookDBretryWrites=true&w=majority&appName=BellevueUniversity"
 
@@ -16,6 +17,31 @@ client = MongoClient(conn, tls=True, tlsAllowInvalidCertificates=True)
 #Creating a variable to access the collection
 db = client['whatabookDB']
 
-#Display all documents in the customers collection
-for customer in db.customer.find({}, {"wishListItems"}):
-    print(customer)
+#Display a list of books
+for book in db.books.find({}):
+    print(f"{book['title']}")
+
+#print('Available Genres:')
+
+#for genres in genre:
+ #   print(genres)
+
+# A variable to store the user's choice
+choice = input('''Select a genre:
+        Fantasy
+        Fiction
+        Romance
+        History
+''')
+
+# Prints the selected choice
+print('You selected ' + choice)
+
+# A blank line
+print('')
+
+# A loop that prints the books by genre based on choice
+for book in db.books.find({"genre": choice}):
+    print('Title:', book.get("title"))
+
+
