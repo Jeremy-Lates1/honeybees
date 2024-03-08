5e7c70eba4bdafd34605b4a36bc166f1f0654ac2
Evelyn Zepeda <evaynnn@gmail.com>
2024-03-07 22:29:48 -0800

    Updates

--- whatabook/honeybee-whatabook.py
+++ whatabook/honeybee-whatabook.py
@@ -9,29 +9,31 @@ from pymongo import MongoClient
 
 
 #A string to connect to the database
-conn = "mongodb+srv://whatabook_admin:s3cret@bellevueuniversity.8vzftv7.mongodb.net/whatabookDBretryWrites=true&w=majority&appName=BellevueUniversity"
+conn = "mongodb+srv://web335_user:s3cret@bellevueuniversity.8vzftv7.mongodb.net/web335DBretryWrites=true&w=majority&appName=BellevueUniversity"
 
 #To create a client
 client = MongoClient(conn, tls=True, tlsAllowInvalidCertificates=True)
 
 #Creating a variable to access the collection
-db = client['whatabookDB']
+db = client['web335DB']
+
+#A label for the displayed books
+print('The following is a list of books:')
+print('')
 
 #Display a list of books
 for book in db.books.find({}):
     print(f"{book['title']}")
 
-#print('Available Genres:')
-
-#for genres in genre:
- #   print(genres)
+print('')
 
 # A variable to store the user's choice
 choice = input('''Select a genre:
         Fantasy
         Fiction
         Romance
-        History
+        Drama
+        Aviation
 ''')
 
 # Prints the selected choice
@@ -44,12 +46,23 @@ print('')
 for book in db.books.find({"genre": choice}):
     print('Title:', book.get("title"))
 
+print('')
+def find_id(id):
+    for customer in db.customer.find({'customerId': id}):
+        print('Wishlist:', customer.get('wishListItems'))
+           
+
+
 
 entered_id = input('Enter you customer ID number to see your wishlist: ')
 
-if entered_id == 'c1001':
-    for customer in db.customer.find({'customerId': entered_id}):
-        print('Wishlist:', customer.get('wishListItems'))
+
+if entered_id == 'c1006':
+    find_id(entered_id)
+elif entered_id == 'c1007':
+    find_id(entered_id)
+elif entered_id == 'c1008':
+    find_id(entered_id)
 else:
     print('Incorrect ID: No customer found.')
     
